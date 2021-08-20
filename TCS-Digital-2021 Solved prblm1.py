df035bad23a88cd8e0d73352e2b1f8fda02ef29e
Karthi M <71722407+karthi517@users.noreply.github.com>
2021-08-20 17:29:02 +0530

    Update TCS-Digital-2021 Solved prblm1.py

--- TCS-Digital-2021 Solved prblm1.py
+++ TCS-Digital-2021 Solved prblm1.py
@@ -4,7 +4,13 @@ in the range interval [n1, n2] [both inclusive] which have no repeated digits.
 For e.g.
 Suppose n1 = 11 and n2 = 15.
 There is the number 11, which has repeated digits, but 12, 13, 14, and 15 have no repeated digits. So, the
-output is 4."""
+output is 4.
+Input Output
+11 -- Value of n1
+15 -- Value of n2 4
+101 -- Value of n1
+200 -- Value of n2 72"""
+
 #####################################################################################################################################
 input1=int(input())
 input2=int(input())
